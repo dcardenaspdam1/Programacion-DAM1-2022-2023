3afa11a7c10da1b76d0d7ce21954b98e1415fd67
Montse Viyuela <montsemsanz@gmail.com>
2022-11-18 16:12:23 +0100

    Añadidos ejemplos explicaciones miembros static

---
+++ UT4/Ejemplos miembros static/Circulo.java
@@ -0,0 +1,49 @@
+
+/**
+ *  
+ * 
+ * Clase que contiene miembros static
+ */
+public class Circulo
+{
+    public static final double PI = 3.1416;
+    private static int contadorCirculos = 0;
+    private double radio;
+    private int numero;
+
+    /**
+     * Constructor  
+     */
+    public Circulo(double radio)   {
+        contadorCirculos ++;
+        this.radio = radio;
+        numero = contadorCirculos;
+
+    }
+
+    /**
+     *    
+     * 
+     */
+    public double getPerimetro()    {
+        return 2 * PI * radio;
+    }
+
+    /**
+     *    
+     * 
+     */
+    public  static int getContadorCirculos()   {
+        
+        return contadorCirculos;
+    }
+
+    /**
+     *  
+     */
+    public String toString()    {
+        return "Círculo: " + this.numero + 
+        "\nRadio:" + radio + "\n";
+    }
+
+}
